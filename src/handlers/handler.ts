119f407f503a436591c17b22067dfb218498c1c5
Alexander Shashkin <alexander@talcura.com>
2019-04-17 00:58:06 -0400

    Refactored getNewUrlWithFileCheck feature

--- src/handlers/handler.ts
+++ src/handlers/handler.ts
@@ -1,9 +1,17 @@
+import * as fs from 'fs';
+
 export class ExtHandler {
     protected regExp: RegExp;
     protected newExt: string;
+    protected fallbackExt: string | null;
 
-    constructor(oldExt: string, newExt: string) {
+    constructor(
+        oldExt: string,
+        newExt: string,
+        fallbackExt: string | null = null
+    ) {
         this.newExt = newExt;
+        this.fallbackExt = fallbackExt;
 
         this.regExp = new RegExp(`${oldExt}$`);
     }
@@ -13,6 +21,20 @@ export class ExtHandler {
             return oldFile.replace(this.regExp, this.newExt);
         } else {
             return null;
+        }
+    }
+
+    public getNewUrlWithFileCheck(oldFile: string, dir: string): string | null {
+        let newFile = this.getNewUrl(oldFile);
+        if (!newFile || !this.fallbackExt) {
+            return null;
+        }
+
+        const path = `${dir}\\${newFile}`;
+        if (fs.existsSync(path)) {
+            return newFile;
+        } else {
+            return oldFile.replace(this.regExp, this.fallbackExt);
         }
     }
 }
--- src/handlers/ts-handler.ts
+++ src/handlers/ts-handler.ts
@@ -1,23 +1,8 @@
 import { ExtHandler } from './handler';
-import * as fs from 'fs';
 
 class TSHandler extends ExtHandler {
     constructor() {
-        super('.ts', '.html');
-    }
-
-    public getNewUrlWithFileCheck(oldFile: string, dir: string): string | null {
-        let newFile = super.getNewUrl(oldFile);
-        if (!newFile) {
-            return null;
-        }
-
-        const path = `${dir}\\${newFile}`;
-        if (fs.existsSync(path)) {
-            return newFile;
-        } else {
-            return oldFile.replace(this.regExp, '.vue.html');
-        }
+        super('.ts', '.html', '.vue.html');
     }
 }
 
